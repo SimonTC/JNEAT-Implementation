de9a53c0b0e4739df4d1e26d7b0f257fe979f398
Simon T. Clement <STClementGit@gmail.com>
2013-11-02 15:55:03 +0100

    Revert "Slettede nogle filer der måske ikke bruges"
    This reverts commit cce9ccce2f99e353c3035a9b402fab3050e85e11.

---
+++ JNEAT/src/jNeatCommon/FolderConstant.java
@@ -0,0 +1,12 @@
+package jNeatCommon;
+/**
+ * This class contains info about where to save and load files
+ * Based on the origininal hardoced savefolders in the code
+ * @author Simon
+ *  30-10-13
+ *
+ */
+public class FolderConstant {
+	public static String MAIN_FOLDER = "C:\\Users\\Simon\\Documents\\MarioFun\\jneat";
+	public static String DATA_FOLDER = "C:\\Users\\Simon\\Documents\\MarioFun\\jneat\\data";
+}
